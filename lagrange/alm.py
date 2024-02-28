42941764fa7622169f1d18afe427089d0340a37b
Abenteuerlustig（Hao <asdasdaf2112ds@gmail.com>
2024-02-28 20:59:50 +0800

    understand the process of augmented lagrange multiplier algorithm based on a simple question

--- lagrange/alm.py
+++ lagrange/alm.py
@@ -11,7 +11,7 @@ import torch
 
 
 def lagrangian_function(x, lambda_):
-    return f(x) + lambda_ @ (A @ x - b) + alpha / 2 * ((A @ x - b)**2).sum()
+    return f(x) + lambda_ @ (A @ x - b) + alpha / 2 * ((A @ x - b) ** 2).sum()
 
 
 def f(x):
@@ -22,7 +22,7 @@ def update_x(x, lambda_):
     """ update x with gradient descent """
     lagrangian_function(x, lambda_).backward()
     new_x = x - eta * x.grad
-    x.data = new_x.clamp(min=0)
+    x.data = new_x.clamp(min=0)  # ensure x not lower than zero
     x.grad.zero_()
 
 
@@ -31,19 +31,19 @@ def update_lambda(lambda_):
     lambda_.data = new_lambda
 
 
-def pprint(i, x, lambda_):
+def pprint(i, x, lambda_, epoch):
     print(
-        f'\n{i+1}th iter, L:{lagrangian_function(x, lambda_):.2f}, f: {f(x):.2f}'
+        f'\n{i}/{epoch}, L:{lagrangian_function(x, lambda_):.2f}, f: {f(x):.2f}'
     )
     print(f'x: {x}')
     print(f'lambda: {lambda_}')
-    print("constraints violation: ")
-    print(A @ x - b)
+    print("constraints violation: " + str(A @ x - b))
 
 
 def solve(x, lambda_):
-    for i in range(500):
-        pprint(i, x, lambda_)
+    epoch = 500
+    for i in range(epoch):
+        pprint(i + 1, x, lambda_, epoch)
         update_x(x, lambda_)
         update_lambda(lambda_)
 
